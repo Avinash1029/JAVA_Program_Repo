8c3441e1a19f64d49c9e558a84c45d0addb9b355
Avinash <Avinash@LAPTOP-ECIJOHCB>
2024-09-07 23:45:30 +0530

    Code changes done

--- src/Array_Programs/Even_Numbers.java
+++ src/Array_Programs/Even_Numbers.java
@@ -4,15 +4,17 @@ public class Even_Numbers {
 	
 	public static void main(String[] args) {
 		
+		// Programme for Even_Numbers
+		
 		int x = 2;
 		
 		if(x%2==0)
 		{
-			System.out.println(x + " is th even number");
+			System.out.println(x + " is the even number");
 		}
 		else
 		{
-			System.out.println(x + " is th odd number");
+			System.out.println(x + " is the odd number");
 		}
 		
 		
