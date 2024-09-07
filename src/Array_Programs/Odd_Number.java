c365391e7e91b8a7dff202deba664b7e6809c431
Avinash <Avinash@LAPTOP-ECIJOHCB>
2024-09-07 23:21:49 +0530

    Odd Number programm Comitted

---
+++ src/Array_Programs/Odd_Number.java
@@ -0,0 +1,21 @@
+package Array_Programs;
+
+public class Odd_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 3;
+		
+		if(x%2==0)
+		{
+			System.out.println(x + " is th even number");
+		}
+		else
+		{
+			System.out.println(x + " is th odd number");
+		}
+
+	}
+
+}
