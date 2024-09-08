a59d44712c16dc959296f23f202f212d7e2ecb29
Avinash <Avinash@LAPTOP-ECIJOHCB>
2024-09-08 20:02:24 +0530

    One String programme added.

--- src/String_Programme/Reverse_String.java
+++ src/String_Programme/Reverse_String.java
@@ -4,7 +4,16 @@ public class Reverse_String {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String s = "Avinash Mali";
+		
+		String rev = "";
+		
+		for(int i=s.length()-1; i>=0; i--)
+		{
+			rev = rev+s.charAt(i);
+		}
+		System.out.println(rev);
 	}
 
 }
