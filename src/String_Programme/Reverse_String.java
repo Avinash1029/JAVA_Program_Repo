34ce60f6f9716d31ee352582e693aff3edfd01a2
Avinash1029 <avinash.f.mali98@gmail.com>
2024-11-06 17:11:20 +0530

    Revert "Pull to Local"

--- src/String_Programme/Reverse_String.java
+++ src/String_Programme/Reverse_String.java
@@ -3,17 +3,8 @@ package String_Programme;
 public class Reverse_String {
 
 	public static void main(String[] args) {
-		
-		
-		String s = "Avinash Mali";
-		
-		String rev = "";
-		
-		for(int i=s.length()-1; i>=0; i--)
-		{
-			rev = rev+s.charAt(i);
-		}
-		System.out.println(rev);
+		// TODO Auto-generated method stub
+
 	}
 
 }
